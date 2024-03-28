7c781c738c0de5c83d0d8453ea279efac841c4d1
Rasmus Abel <rasmus2@abelcall.de>
2024-03-28 14:32:57 +0100

    Löschen von Touren und Attraktionen jetzt möglich

--- microservice/src/main/java/de/bird/vogelpark/service/delete/DeleteTourService.java
+++ microservice/src/main/java/de/bird/vogelpark/service/delete/DeleteTourService.java
@@ -1,6 +1,8 @@
 package de.bird.vogelpark.service.delete;
 
 import de.bird.vogelpark.beans.Tour;
+import de.bird.vogelpark.beans.Attraction;
+import de.bird.vogelpark.repositories.AttractionRepository;
 import de.bird.vogelpark.repositories.TourRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,10 +14,12 @@ import java.util.Optional;
 public class DeleteTourService {
 
     private final TourRepository tourRepository;
+    private final AttractionRepository attractionRepository;
 
-
-    public DeleteTourService(TourRepository tourRepository) {
+    public DeleteTourService(TourRepository tourRepository,
+                             AttractionRepository attractionRepository) {
         this.tourRepository = tourRepository;
+        this.attractionRepository = attractionRepository;
     }
 
     public ResponseEntity<String> deleteTour(String tourName){
@@ -25,6 +29,13 @@ public class DeleteTourService {
         if(foundTour.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
                     .body(String.format("Tour %s does not exist and can't be deleted", tourName));
+        }
+
+        //Tour aus allen beteiligten Attraktionen entfernen,
+        //damit keine Referenzen auf die zu löschende Tour mehr existieren
+        for(Attraction a : foundTour.get().getAttractions()) {
+            a.getTours().remove(foundTour.get());
+            attractionRepository.save(a);
         }
 
         //Wenn ja, dann Attraktion löschen
